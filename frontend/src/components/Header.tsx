f20e8c09d8489c2413ece161072cd6c06d731296
sonlh02 <let02hb@gmail.com>
2024-03-13 21:04:52 +0700

    Update Desktop Nav, Added Responsive Landing Page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -12,7 +13,10 @@ const Header = () => {
           HSonEats.com
         </Link>
         <div className="md:hidden">
-            <MobileNav/>
+          <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,3 +1,14 @@
-const MainNav = () => {};
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-white"
+    >
+      Đăng nhập
+    </Button>
+  );
+};
 
 export default MainNav;
