65421d8ff375934db2047259ba6ad7198b4bf893
sonlh02 <let02hb@gmail.com>
2024-03-17 22:54:24 +0700

    Updated User Api, Created User Request

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -8,6 +8,15 @@ const createCurrentUser = async (req: Request, res: Response) => {
     try{
         const {auth0Id} = req.body;
         const existingUser = await User.findOne({auth0Id});
+
+        if(existingUser){
+            return res.status(200).send();
+        }
+
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.status(201).json(newUser.toObject());
     }catch(error){
         console.log(error);
         res.status(500).json({message: "Server error"});
