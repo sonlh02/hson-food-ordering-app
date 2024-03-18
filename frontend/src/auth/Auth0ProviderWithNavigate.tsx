c0995aae0cfc2ec66ace310a2dba7cd7be877725
sonlh02 <let02hb@gmail.com>
2024-03-18 17:29:28 +0700

    Updated User Request, Setuped Auth0 Access Token, Validate Token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,12 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -12,8 +14,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   if (!domain || !clientId || !redirectUri) {
     throw new Error("unable to initialise auth");
   }
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("USER", user);
+  const onRedirectCallback = () => {
+    navigate("/auth-callback");
   };
 
   return (
