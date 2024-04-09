885d78dfac10786e7875c3b22d38bce475b1ea66
sonlh02 <let02hb@gmail.com>
2024-04-09 17:02:41 +0700

    Pre-populated user profile form

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -2,14 +2,24 @@ import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  const { currentUser, isLoading: isGetLoading} = useGetMyUser();
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
   const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
-  if(isGetLoading){
+  if (isGetLoading) {
     return <span>Loading...</span>;
   }
 
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 };
 
 export default UserProfilePage;
